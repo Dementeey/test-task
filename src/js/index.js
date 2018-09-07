5dcbe6013cb39fa3e5161a41239000f094328206
dementeeyMac <metallist9656@gmail.com>
2018-09-07 11:43:26 +0300

    add resoinsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,21 @@
 const goToTop = document.querySelector('.page-footer__go-to-top');
+const burger = document.querySelector('.burger__wrap');
 
-goToTop.addEventListener('click', () => {
-	document.documentElement.scrollTop = 0
-});
+const burgerShow = () => {
+  const burger = document.querySelector('.burger');
+  const pageNav = document.querySelector('.page-nav');
+
+  if (burger.classList.contains('burger--active') || pageNav.classList.contains('page-nav--show')) {
+    burger.classList.remove('burger--active');
+    pageNav.classList.remove('page-nav--show');
+    return;
+  }
+
+  burger.classList.add('burger--active');
+  pageNav.classList.add('page-nav--show');
+  return;
+}
+
+goToTop.addEventListener('click', () => document.documentElement.scrollTop = 0);
+burger.addEventListener('click', burgerShow);
+
